3a94061b234d8ec36436fe11853e294d01d77f54
Pankaj <pankaj.kumar.phe21@itbhu.ac.in>
2024-06-05 21:37:16 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express";
+const app=express()
+import http from "http";
+import cors from "cors";
+import { Server } from "socket.io";
+
+app.use(cors());
+const server=http.createServer(app);
+
+const io=new Server(server,{
+    cors:{
+        origin:"http://localhost:3000",
+        methods:["GET","POST"],
+    }
+})
+
+io.on("connection",(socket)=>{
+    socket.on("join_room",(data)=>{
+        socket.join(data);
+    })
+    socket.on("send_message",(data)=>{
+        socket.to(data.room).emit("receive_message",data)
+    })
+})
+
+server.listen(process.env.PORT || 3001)
