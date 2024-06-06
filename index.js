aa3d2c3286fe34aafc467b4c3085a9763a3cf9dd
Pankaj <pankaj.kumar.phe21@itbhu.ac.in>
2024-06-06 07:58:50 +0530

    c

--- index.js
+++ index.js
@@ -2,19 +2,29 @@ import express from "express";
 const app=express()
 import http from "http";
 import cors from "cors";
+import { createProxyMiddleware } from 'http-proxy-middleware';
 import {Server} from "socket.io";
 
 app.use(cors({
-    origin: 'https://chat-client-one-sable.vercel.app',
+    origin: 'http://localhost:3000',
     methods: ['GET', 'POST'],
     credentials: true
+}));
+
+app.use('/socket.io', createProxyMiddleware({
+    target: 'http://localhost:3000',
+    changeOrigin: true,
+    ws: true,
+    onProxyReq: (proxyReq, req, res) => {
+        proxyReq.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    }
 }));
 
 const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: 'https://chat-client-one-sable.vercel.app',
+        origin: 'http://localhost:3000',
         methods: ['GET', 'POST'],
         credentials: true
     }
@@ -26,6 +36,9 @@ io.on("connection",(socket)=>{
     })
     socket.on("send_message",(data)=>{
         socket.to(data.room).emit("receive_message",data)
+    })
+    app.get('/:name',(req,res)=>{
+        res.send(req.params.name)
     })
 })
 
