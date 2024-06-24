7fb9942c9796b43de0f48f5f27daf985308b42ce
Pankaj <pankaj.kumar.phe21@itbhu.ac.in>
2024-06-24 14:33:43 +0530

    h

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ import cors from "cors";
 import {Server} from "socket.io";
 
 app.use(cors({
-    origin: 'http://localhost:3000/',
+    origin: 'http://localhost:3000',
 }));
 
 const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: 'http://localhost:3000/',
+        origin: 'http://localhost:3000',
         methods: ['GET', 'POST'],
         credentials: true
     }
