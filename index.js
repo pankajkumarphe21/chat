75f7b818dbce22cdc4028a601dc2733f07a0c16f
Pankaj <pankaj.kumar.phe21@itbhu.ac.in>
2024-06-24 14:32:55 +0530

    h

--- index.js
+++ index.js
@@ -2,18 +2,17 @@ import express from "express";
 const app=express()
 import http from "http";
 import cors from "cors";
-import { createProxyMiddleware } from 'http-proxy-middleware';
 import {Server} from "socket.io";
 
 app.use(cors({
-    origin: '*',
+    origin: 'http://localhost:3000/',
 }));
 
 const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: '*',
+        origin: 'http://localhost:3000/',
         methods: ['GET', 'POST'],
         credentials: true
     }
